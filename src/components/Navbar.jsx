d4094fccd70c51d9934e377800e2f81f8d671e8e
sudarshanmane <sudarshanmane2110@gmail.com>
2023-08-24 14:22:59 +0530

    work in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
-import { Avatar, Button, Segmented, Space } from "antd";
+import { Button, Drawer, Segmented, Space } from "antd";
 import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import "../Images/Logo.png";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
-import About from "./About";
+import {
+  HomeOutlined,
+  UserOutlined,
+  ToolOutlined,
+  ProjectOutlined,
+  AreaChartOutlined,
+  ContactsOutlined,
+  CloseOutlined,
+  DatabaseOutlined,
+} from "@ant-design/icons";
 
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   let [selectedPage, setSelectedPage] = useState([]);
+  let baseUrl = `/ReactPortfolio`;
 
   useEffect(() => {
     const skills = location.pathname;
@@ -19,9 +28,17 @@ const Navbar = () => {
 
   function changed(value) {
     setSelectedPage(value);
-
-    if (value !== "resume") navigate(`/ReactPortfolio/${value}`);
+    if (value !== "resume") navigate(`${baseUrl}/${value}`);
   }
+
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="navbar_container">
       <Space className="portfolio_log_image">
@@ -29,7 +46,65 @@ const Navbar = () => {
           <img src={require("../Images/Logo.png")} alt="logo" />
         </Link>
       </Space>
-      <Space direction="vertical">
+
+      <Space className="dropdown-container">
+        <Space>
+          <Button type="primary" onClick={showDrawer}>
+            Open
+          </Button>
+        </Space>
+        <Drawer
+          title="Basic Drawer"
+          placement={"right"}
+          closable={false}
+          onClose={onClose}
+          open={open}
+          key={"right"}
+          extra={
+            <Space>
+              <Button
+                type="text"
+                style={{ color: "green", fontSize: "20px" }}
+                onClick={onClose}
+              >
+                <CloseOutlined></CloseOutlined>
+              </Button>
+            </Space>
+          }
+        >
+          <Space direction="vertical">
+            <div onClick={() => changed("home")}>
+              <HomeOutlined></HomeOutlined> Home
+            </div>
+            <div onClick={() => changed("about")}>
+              <UserOutlined></UserOutlined>
+              About Me
+            </div>
+            <div onClick={() => changed("skills")}>
+              <ToolOutlined />
+              Skills
+            </div>
+            <div onClick={() => changed("projects")}>
+              {" "}
+              <ProjectOutlined />
+              Projects
+            </div>
+            <div onClick={() => changed("statistics")}>
+              <AreaChartOutlined />
+              Statistics
+            </div>
+            <div onClick={() => changed("contact")}>
+              {" "}
+              <ContactsOutlined></ContactsOutlined>Contact
+            </div>
+            <div onClick={() => changed("resume")}>
+              <DatabaseOutlined />
+              Resume
+            </div>
+          </Space>
+        </Drawer>
+      </Space>
+      <Space direction="vertical" className="navbar-elements-space">
         <Segmented
           value={selectedPage}
           className="custom-segmented"
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+      className="project-container"
+    >
+      <h1 style={{ backgroundColor: "black" }}>Projects workin on it</h1>
+    </div>
+  );
 };
 
 export default Projects;
